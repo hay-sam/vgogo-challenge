21c57b947681af3d313134c06cd05230ac068ab1
Haley Sambursky <hsambursky46@gmail.com>
2020-01-08 11:50:12 -0500

    one event

--- src/SelectedEvent.js
+++ src/SelectedEvent.js
@@ -5,9 +5,49 @@ const SelectedEvent = (props) => {
   const {event} = props
   return (
     <div className="app-section">
-      <div className="event-item-title">
-        {event.EventName} at {event.VenueName}
+      <div id="selected">
+        <div id="event-title">
+          <div id="event-artist">{event.EventName}</div>
+          <div id="event-venue">
+            Live at the {event.VenueName}
+          </div>
+        </div>
+        <div id="event-date">
+        <div className="event-date">
+        {event.Day+ " "}
+            {event.Month} {event.CalendarViewModel.DayOfMonth},{" "+event.CalendarViewModel.Year}
+          </div>
+        </div>
+        <div id="event-info"></div>
       </div>
+     {/* <div className="event-item-info">
+        <div className="event-item-title">
+          {event.EventName} at {event.VenueName}
+        </div>
+        <div className="event-item-location">
+          {event.VenueCity},{event.VenueCountry}
+        </div>
+        {(event.AvailableTickets > 0)?
+          <div className="green">
+            Tickets Available from {event.MinPrice}
+          </div>
+          :
+          <div className="red">
+            Sold Out
+          </div>
+        }
+        </div>
+        <div className="event-date">
+          <div className="month">
+            {event.Day}
+          </div>
+          <div className="day">
+            {event.Month} {event.CalendarViewModel.DayOfMonth}
+          </div>
+          <div className="year">
+            {event.CalendarViewModel.Year}
+          </div>
+      </div> */}
     </div>
   )
 }
